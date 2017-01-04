eada6a30c98f59f6330d1b9aa9cbd09fb5d679fe
Gonzalo Larralde <gonzalolarralde@gmail.com>
2017-01-04 19:11:26 -0300

    Add new thin image to only distribute the output directory and the dependencies

---
+++ util/thin_image/environment-profile.sh
@@ -0,0 +1,7 @@
+export SR_SWIFT_SDK=/root/output
+export SR_SWIFT_BIN=$SR_SWIFT_SDK/swift-android/usr/bin
+export SR_ANDROID_ARMV7A_SDK=$SR_SWIFT_SDK/android-toolchain/sysroot
+export SR_ANDROID_ARMV7A_TOOLCHAIN=$SR_SWIFT_SDK/android-toolchain/arm-linux-androideabi
+export SR_ANDROID_ARMV7A_GCC_LIB=$SR_SWIFT_SDK/android-toolchain/lib/gcc/arm-linux-androideabi/4.9.x
+export PATH=$PATH:$SR_SWIFT_BIN
+
