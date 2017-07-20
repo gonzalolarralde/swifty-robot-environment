981eaf8258fc74557a7c16fe28441936801b46f6
Gonzalo Larralde <gonzalolarralde@gmail.com>
2017-07-19 23:50:46 -0300

    Docker Image version bumped and environment updated.
    New version based on Jul 14, 2017 apple/swift master.
    
    ~/swift-source/clang 1c8a29ef21091cf53d84c62494563487a6d9f139
    ~/swift-source/cmark d875488a6a95d5487b7c675f79a8dafef210a65f
    ~/swift-source/compiler-rt fc3ea6b314fb2ae6f7b18f40ed9897dd8a9541cf
    ~/swift-source/llbuild 1370ca71339b0c2a01d660834f83f22c94845633
    ~/swift-source/lldb ada40dc6575ead0b0bf182d2d078bf31904ce2ed
    ~/swift-source/llvm 60522348b3228984cc880f5ff9c29a98b58336a2
    ~/swift-source/ninja 256bf897b85e35bc90294090ad39b5214eb141fb
    ~/swift-source/swift 07f5b8d646a13edd69f5f16e475fc7ea5e0c065b
    ~/swift-source/swift-corelibs-foundation
    ce1a76c081f4b0bb98a33d98e595db95c7a4c871
    ~/swift-source/swift-corelibs-libdispatch
    f8e71eb40e53863fc908fe3afc3e6078018cfa3c
    ~/swift-source/swift-corelibs-xctest
    90ac02ae746c0776e7e37e4a00db7b38040d9116
    ~/swift-source/swift-integration-tests
    1d5d149f7aab027c9a7dccd19c0680bf36761a68
    ~/swift-source/swift-xcode-playground-support
    05737c49f04b9089392b599ad529ab91c7119a75
    ~/swift-source/swiftpm 74f826bbf0ae3f264ae1e73b7fe09925a74384fe

--- util/thin_image/environment-profile.sh
+++ util/thin_image/environment-profile.sh
@@ -1,7 +1,6 @@
-export SR_SWIFT_SDK=/root/output
-export SR_SWIFT_BIN=$SR_SWIFT_SDK/swift-android/usr/bin
-export SR_ANDROID_ARMV7A_SDK=$SR_SWIFT_SDK/android-toolchain/sysroot
-export SR_ANDROID_ARMV7A_TOOLCHAIN=$SR_SWIFT_SDK/android-toolchain/arm-linux-androideabi
-export SR_ANDROID_ARMV7A_GCC_LIB=$SR_SWIFT_SDK/android-toolchain/lib/gcc/arm-linux-androideabi/4.9.x
+export SR_SWIFT_SDK=/root/swift-install
+export SR_SWIFT_BIN=$SR_SWIFT_SDK/usr/bin
+export SR_ANDROID_ARMV7A_SDK=/root/android-standalone-toolchain/sysroot
+export SR_ANDROID_ARMV7A_TOOLCHAIN=/root/android-standalone-toolchain/arm-linux-androideabi
+export SR_ANDROID_ARMV7A_GCC_LIB=/root/android-standalone-toolchain/lib/gcc/arm-linux-androideabi/4.9.x
 export PATH=$PATH:$SR_SWIFT_BIN
-
